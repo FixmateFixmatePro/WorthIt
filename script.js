f2bc28e5c42749dd63bfb838e58b2cbf3cfa023e
FixmateFixmatePro <FixMateAITradesTool@gmail.com>
2025-07-02 01:59:33 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector("button");
+  btn.addEventListener("click", () => {
+    alert("Thanks for joining the WorthIt beta!");
+  });
+});
